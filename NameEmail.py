dfa9e2f00e2715e5f9bba0646f101bc90440df5d
Khaled_Alghamdi <khaled.alghamdi0@hotmail.com>
2023-07-26 11:47:13 +0300

    Short version for bouns

--- NameEmail.py
+++ NameEmail.py
@@ -22,15 +22,13 @@ the name length must be more than 2 characters, please provide a valid name.
 """
 
 name =input("Enter your name : ")
-email=input('Enter your email : ')
-valid_email= email.find("@gmail")!=-1
-valid_name=len(name)>2
-if (valid_email) and (valid_name):
-    print(f"Welcome {name} you registered with the email {email} !")
-elif (valid_email == False):
-    print(" the email is not valid , please provide a valid email .")
-else :
-    print('the name length must be more than 2 characters, please provide a valid name.')
+if len(name)<=2:
+       print('the name length must be more than 2 characters, please provide a valid name.')
+else:
+    email=input('Enter your email : ')
+    if email.find("@gmail")!=-1:
+             print(f"Welcome {name} you registered with the email {email} !")
+    else:
+            print(" the email is not valid , please provide a valid email .")
 
-
-
+         
