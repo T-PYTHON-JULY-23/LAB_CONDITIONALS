f40e16e8d80c24c9a45bde7e1b753c1d77cd2f44
renad1 <renadxx17@gmail.com>
2023-07-26 12:54:34 +0300

    bonus

--- bonus.py
+++ bonus.py
@@ -2,9 +2,10 @@ print("Sign up")
 print("-"*20)
 name= input("Enter your name: ")
 email= input("Enter your email: ")
-if not len(name) > 2:
+if  len(name) <= 2:
     print('the name length must be more than 2 characters, please provide a valid name.')
 if not email.count('@gmail'):
     print('the email is not valid , please provide a valid email')
 if len(name) > 2 and email.count('@gmail'):
     print(f'welcome {name}, you registered with the email {email} !')
+
