14c1da00ff696413a2a22e56c49c5a162b78ef2c
ghaida <ghaida@gmail.com>
2023-07-26 13:11:51 +0300

    bouns

--- bouns.py
+++ bouns.py
@@ -3,7 +3,7 @@ email= input("inter your email:")
 if len(name)<=2:
     print(f"the name length must be more than 2 characters, please provide a valid name.")
 
-elif email.index("@gmail") :
+elif "@gmail" in email :
     print(f"welcome {name}, you registered with the email {email} !")
 else:
     print(" the email is not valid , please provide a valid email .")
