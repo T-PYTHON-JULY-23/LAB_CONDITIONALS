9ef098c2860e9c77ee452c834cf527825f95923c
MaramFahad <maramll98ll@gmail.com>
2023-07-26 11:52:17 +0300

    assigment solved

---
+++ ConditionalsLab.py
@@ -0,0 +1,17 @@
+
+fav_movie="Baby Draiver"
+
+movie_rating= int(1)
+
+popularity= float(80)
+
+
+if movie_rating >= 4 and popularity >80:
+    print("higly recommended")
+elif movie_rating >= 3 and popularity > 70:
+    print("I recommended it . It is good")
+elif movie_rating <= 2 and popularity> 60:
+    print("You should check it out!") 
+else: 
+    movie_rating <=2 and popularity <50
+    print("Don't watch it. It is a waste of time")
