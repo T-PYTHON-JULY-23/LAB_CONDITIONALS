52ea5f519d09295dfe15f09b7783a5884defc8b4
Lenah11 <lenhh11@gmail.com>
2023-07-26 11:41:48 +0300

    Lab_Conditionals_Lenah

---
+++ LAB_CONDITIONALS.py
@@ -0,0 +1,18 @@
+#Create a variable for the movie (choose any movie you like)
+My_movie = "Dangal"
+rating = int(3)
+popularity_score = float(72.65)
+
+
+#Using an if statement
+
+if rating >= 4 and popularity_score > 80:
+    print("Highly recommended")
+elif rating >= 3 and popularity_score > 70:
+    print("I recommended it . It is good")
+elif rating >= 2 and popularity_score > 60:
+    print("You should check it out!")
+else:
+    print("Don't watch it. It is a waste of time")
+
+
