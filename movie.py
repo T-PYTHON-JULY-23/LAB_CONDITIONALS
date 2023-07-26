46d86f449b87793d6d128ad681cea5d8cb98e9c8
Atheer Abdullah <Atheer98.abdullah19@gmail.com>
2023-07-26 12:05:37 +0300

    finished

---
+++ movie.py
@@ -0,0 +1,19 @@
+movie_name = "Oppenheimer"
+rating_of_the_movie = 3 
+popularity_score = 72.65
+
+
+
+if (rating_of_the_movie >= 4) and (popularity_score > 80):
+    print("Highly recommended")
+
+elif (rating_of_the_movie >= 3) and (popularity_score > 70):
+     print("I recommended it . It is good")
+
+
+elif (rating_of_the_movie <= 2) and (popularity_score > 60):
+     print("ou should check it out!")
+
+elif (rating_of_the_movie <= 2) and (popularity_score < 50):
+     print("Don't watch it. It is a waste of time")
+
